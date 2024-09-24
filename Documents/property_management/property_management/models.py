60d9a647875d186f55f2fa20b6eddf4706b2bf5a
Fatima0717 <olayaescavias@gmail.com>
2024-09-24 17:53:26 +0900

    Fixes for the project

--- Documents/property_management/property_management/models.py
+++ Documents/property_management/property_management/models.py
@@ -1,9 +1,10 @@
+# properties/models.py
+from django.contrib.auth.models import User
 from django.db import models
 
-class Property(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_properties = models.ManyToManyField('Property', blank=True)
 
     def __str__(self):
-        return self.title
+        return self.user.username
--- Documents/property_management/property_management/properties/admin.py
+++ Documents/property_management/property_management/properties/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Property
 
-# Register your models here.
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'address')
+    search_fields = ('name', 'address')
+    list_filter = ('type',)  # Assuming you have a 'type' field
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'price', 'address', 'description', 'type', 'available'),
+        }),
+    )
+    
+    actions = ['mark_as_sold']
+
+    def mark_as_sold(self, request, queryset):
+        queryset.update(status='sold')
+    mark_as_sold.short_description = "Mark selected properties as sold"
